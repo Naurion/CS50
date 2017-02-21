4fa83112492df61013d1a8c474d2e6bcfec0df12
naurion <naurion@yandex.ru>
2017-02-21 23:51:11 +0300

    change

--- src/CS50/CheckAlpha.java
+++ src/CS50/CheckAlpha.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 public class CheckAlpha {
     public static Boolean Check(char a) throws IOException {
         Boolean bul = false;
-        BufferedReader br = new BufferedReader (new FileReader("d:\\Java\\Projects\\src\\CS50\\Alphabet"));
+        BufferedReader br = new BufferedReader (new FileReader("src/CS50/Alphabet"));
         String str = br.readLine();
         for (int i=0; i<str.length();i++){
             //System.out.println(str.charAt(i));
