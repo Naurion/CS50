f510096cc874a0aca6aaa11d01785a259f696fc6
naurion <B2edfnfh>
2017-04-21 17:36:00 +0400

    JavaFX 0.1

--- src/Fifteen/Visual.java
+++ src/Fifteen/Visual.java
@@ -1,22 +1,67 @@
 package Fifteen;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.*;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class Visual extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+    static int mMax = 0;
+    static int counter = 0;
+    double mX, mY;
+    HashMap<Integer,Button> buttons = new HashMap<Integer, Button>();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Fifteen");
-        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.setTitle("Fefteen");
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(10);
+        root.setVgap(10);
+        root.setPadding(new Insets(10, 10, 10, 10));
+        primaryStage.setScene(new Scene(root));
+
+        for (int i = 0; i < mMax*mMax; i++) {
+            buttons.put(i, new Button());
+            buttons.get(i).setText("" + i);
+            buttons.get(i).setId("" + i);
+            buttons.get(i).setPrefSize(40, 40);
+        }
+
+        for (int i = 0; i < buttons.size(); i++) {
+            String mButtonId = buttons.get(i).getId();
+            buttons.get(i).setOnAction(new EventHandler<ActionEvent>() {
+
+                @Override
+                public void handle(ActionEvent event) {
+                    VisualMove.mVisualMove(root);
+                 }
+            });
+        }
+
+        buttons.get(0).setVisible(false);
+
+        for (int i = 0; i < mMax; i++) {
+            for (int j = 0; j < mMax; j++) {
+                root.add(buttons.get(counter++), j , i);
+            }
+        }
         primaryStage.show();
     }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        mMax = Integer.parseInt(reader.readLine());
+        launch(args);
+    }
+
 }
---
+++ src/Fifteen/VisualMove.java
@@ -0,0 +1,25 @@
+package Fifteen;
+
+import javafx.animation.TranslateTransition;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+import java.util.HashMap;
+
+public class VisualMove {
+
+    public static void mVisualMove(GridPane gridPane){
+        int mRow0 = gridPane.getRowIndex(buttons.get(0));
+        int mColumn0 = gridPane.getColumnIndex(buttons.get(0));
+        int mRow = gridPane.getRowIndex(buttons.get(mButtonId));
+        int mColumn = gridPane.getColumnIndex(buttons.get(mButtonId));
+        if(mRow ==mRow0++||mRow ==mRow0--||mColumn ==mColumn0++||mColumn ==mColumn0--)
+        {
+            root.setRowIndex(buttons.get(0), 6);
+            root.setColumnIndex(buttons.get(0), 6);
+            root.setRowIndex(buttons.get(mButtonId), 5);
+            root.setColumnIndex(buttons.get(mButtonId), 5);
+        }
+
+    }
+}
